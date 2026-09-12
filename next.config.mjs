58c8765028947d8cbf9b5ca1091dcd2c6f385ef7
v557639 <shfree@ymail.com>
2026-09-12 22:57:36 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@resvg/resvg-js'],
+  serverComponentsExternalPackages: ['@resvg/resvg-js'],
 };
 
 export default nextConfig;
